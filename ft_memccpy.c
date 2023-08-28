7bd063e3063360025328654a07a155f6e68aa54b
Xale <xale@air-de-axel.home>
2023-08-28 23:42:07 +0200

    memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xale <xale@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/28 23:26:37 by xale              #+#    #+#             */
+/*   Updated: 2023/08/28 23:41:21 by xale             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void    *ft_memccpy(void *dest, const void *src, int c, size_t n)
+{
+    int i;
+
+    i = 0;
+    if (!src && !dest)
+		return (0);
+    while(((char *)dest)[i] != c && ((char *)dest)[i] != '\0' && i < n)
+    {
+        ((char *) dest)[i] = ((char *) src)[i];
+		i++;
+    }
+    return (dest);
+}
