8ee4041f5debd2574f27ea60a478e5247405c698
Xale <xale@air-de-axel.home>
2023-08-28 23:47:15 +0200

    norm

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: xale <xale@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/28 23:26:37 by xale              #+#    #+#             */
-/*   Updated: 2023/08/28 23:45:19 by xale             ###   ########.fr       */
+/*   Updated: 2023/08/28 23:46:48 by xale             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memccpy(void *dest, const void *src, int c, size_t n)
 	i = 0;
 	if (!src && !dest)
 		return (0);
-	while(((char *)dest)[i] != c && ((char *)dest)[i] != '\0' && i < n)
+	while (((char *)dest)[i] != c && ((char *)dest)[i] != '\0' && i < n)
 	{
 		((char *) dest)[i] = ((char *) src)[i];
 		i++;
