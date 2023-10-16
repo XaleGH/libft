4d5987fb2df1c09d49276617b865586624e64784
Xale <xale@MacBook-Air-de-Axel.local>
2023-10-16 14:59:48 +0200

    correctlast

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: asaux <asaux@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/16 14:34:28 by asaux             #+#    #+#             */
-/*   Updated: 2023/10/16 14:56:01 by asaux            ###   ########.fr       */
+/*   Updated: 2023/10/16 14:59:24 by asaux            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
+	if (!lst)
+		return (NULL);
 	while (lst->next)
 	{
 		lst = lst->next;
